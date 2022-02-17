c0a524587693b115e55c443a4f6d4afeccf888a5
Renato Sánchez <renatosancheznevarez@gmail.com>
2022-02-16 18:30:16 -0600

    Routing

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import FullPost from './FullPost';
-import Home from './Home';
+import React from "react";
+import ReactDOM from "react-dom";
+import { PostContextProvider } from "./context/post-context";
+import Post from "./pages/Post";
+import Home from "./pages/Home";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import NotFound from "./pages/NotFound";
+//Aqui va mi contexto y mi router
 
 ReactDOM.render(
   <React.StrictMode>
-    <Home />
-    <FullPost />
+    <PostContextProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/post/:id" element={<Post />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </PostContextProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
