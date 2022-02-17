de84f39952e15c0b82371f0387fe832387d1f37f
Renato Sánchez <renatosancheznevarez@gmail.com>
2022-02-16 18:28:52 -0600

    Updating Filter with MUI

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -4,21 +4,29 @@ const Filter = () => {
   return (
     <Box
       sx={{
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        '& > *': {
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        "& > *": {
           m: 1,
         },
       }}
     >
-      <ButtonGroup variant="outlined" aria-label="outlined button group">
-        <Button>One</Button>
-        <Button>Two</Button>
-        <Button>Three</Button>
+      <ButtonGroup
+        sx={{ flexWrap: "wrap" }}
+        variant="outlined"
+        size="small"
+        aria-label="outlined small button group"
+      >
+        <Button>All</Button>
+        <Button>Travel</Button>
+        <Button>LifeStyle</Button>
+        <Button>Business</Button>
+        <Button>Food</Button>
+        <Button>Work</Button>
       </ButtonGroup>
     </Box>
   );
-}
+};
 
 export default Filter;
