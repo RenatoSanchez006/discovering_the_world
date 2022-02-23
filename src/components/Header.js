9d269fdbc7a215128fa07b04b115e93354d6d152
Renato Sánchez <renatosancheznevarez@gmail.com>
2022-02-23 10:03:15 -0600

    Quick Fix

--- src/components/Header.js
+++ src/components/Header.js
@@ -14,7 +14,12 @@ const Header = () => {
         <Typography color={orange[400]} align="center" variant="subtitle1">
           [Making your Life Easier]
         </Typography>
-        <Typography color={grey[800]} align="center" variant="h4" sx={{fontWeight: 'bold'}}> 
+        <Typography
+          color={grey[800]}
+          align="center"
+          variant="h4"
+          sx={{ fontWeight: "bold" }}
+        >
           Discovering the World
         </Typography>
       </Link>
